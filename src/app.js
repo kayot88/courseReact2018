f3fea0bb77fec5b3a480e439d98f0efb48e74240
Kayot88 <kayot88@gmail.com>
2019-12-08 00:17:13 +0200

    react-select expl

--- src/app.js
+++ src/app.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
+import Select from 'react-select'
+import UserForm from './components/user-form'
 import ArticleList from './components/article-list'
 import articles from './fixtures'
 
 class App extends Component {
+  state = {
+    openItem: null
+  }
   render() {
     return (
       <div>
-
-        <ArticleList articles={articles} />
+        <UserForm />
+        <Select
+          options={this.options}
+          value={this.state.openItem}
+          onChange={this.handleSelect}
+        />
+        <ArticleList articles={articles} ref={this.setArticleListRef} />
       </div>
     )
+  }
+  get options() {
+    return articles.map((article) => ({
+      label: article.title,
+      value: article.id
+    }))
+  }
+  handleSelect = (openItem) => {
+    this.setState({ openItem })
+    return console.log(this.state)
+  }
+  setArticleListRef = (ref) => {
+    console.log('---', ref, findDOMNode(ref))
   }
 }
 
